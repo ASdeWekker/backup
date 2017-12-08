04168299d05923265d5acae0948c9ee69f6e7042
Alex de Wekker <alexdewekker6@gmail.com>
2017-12-08 06:56:42 +0100

    added a config.rb

---
+++ projects/speedtest/config.rb
@@ -0,0 +1,9 @@
+require "compass/import-once/activate"
+
+http_path = "/"
+css_dir = "public/styles"
+sass_dir = "scss"
+images_dir = "public/img"
+javascript_dir = "public/js"
+
+line_comments = false
