0a41f08cb569e472f109863b833a0f7aabc750cb
Alex de Wekker <alexdewekker6@gmail.com>
2018-05-23 22:31:20 +0200

    did somethings, didn't work and added a to do.

--- projects/speedtest/speedtest.js
+++ projects/speedtest/speedtest.js
@@ -5,6 +5,11 @@
 // DONE == Debug photo styling issue chrome mobile.
 // Add some test info from the db.
 // html height is bigger, set no scroll and change body height.
+//
+// Get two extra divs, div1 > div2 > .container div2 is bigger than div1
+// Maybe div2 and container are equals and div2 spans the entire page
+// plus 60 pixels for the chrome bar.
+// div2 is 60 pixels longer than div1 and div1 is overflow hidden.
 
 
 // -------------------- SET UP STUFF --------------------
