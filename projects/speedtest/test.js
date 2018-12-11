07eefbc12b2919220630f036f2ad89fe2e816d3c
Alex de Wekker <alexdewekker6@gmail.com>
2018-12-11 20:35:44 +0100

    added some new packages and made a test db file

--- projects/speedtest/test.js
+++ projects/speedtest/test.js
@@ -1,4 +1,22 @@
 #!/bin/node
 
-const pg 
+const { Client } = require("pg");
+const { execFile } = require("child_process");
 
+const user = process.env.PSQLU;
+const ww = process.env.PSQLW;
+const host = "localhost";
+const psqlport = 5432;
+const db = "test1";
+const connStr = "postgresql://" + user + ":" + ww + "@" + host + ":" + psqlport + "/" + db;
+
+const client = new Client({
+    connectionString: connStr
+});
+client.connect();
+
+const query = "select * from things;";
+
+client.query(query)
+    .then(data => console.log(data.rows))
+    .catch(e => console.error(e.stack));
