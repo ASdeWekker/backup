d4e5db63cd60538ebfd6a79142f2930eba198f13
Alex de Wekker <alexdewekker6@gmail.com>
2018-12-11 20:52:52 +0100

    added the pg module and finished the test test.js

--- projects/speedtest/test.js
+++ projects/speedtest/test.js
@@ -1,22 +1,38 @@
 #!/bin/node
 
-const { Client } = require("pg");
-const { execFile } = require("child_process");
+const { Client, Pool } = require("pg")
+const { execFile } = require("child_process")
 
-const user = process.env.PSQLU;
-const ww = process.env.PSQLW;
-const host = "localhost";
-const psqlport = 5432;
-const db = "test1";
-const connStr = "postgresql://" + user + ":" + ww + "@" + host + ":" + psqlport + "/" + db;
+const user = process.env.PSQLU
+const ww = process.env.PSQLW
+const host = "localhost"
+const psqlport = 5432
+const db = "test1"
+const connStr = "postgresql://" + user + ":" + ww + "@" + host + ":" + psqlport + "/" + db
 
-const client = new Client({
+const pool = new Pool({
     connectionString: connStr
-});
-client.connect();
+})
+// const pool = new Pool({
+//     user: user,
+//     host: host,
+//     database: db,
+//     password: ww,
+//     port: psqlport,
+// })
+//client.connect()
 
-const query = "select * from things;";
+const query = "select * from things"
 
-client.query(query)
-    .then(data => console.log(data.rows))
-    .catch(e => console.error(e.stack));
+pool.query(query, (err, res) => {
+    if (err) {
+        console.log(err)
+    }
+    // console.log(res.rows[0].id)
+    for (let i = 0;i < 3;i++) {
+        console.log(res.rows[i].id)
+        console.log(res.rows[i].name)
+        console.log(res.rows[i].number)
+    }
+    pool.end()
+})
