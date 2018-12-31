9b9d3ee5f2d972738c3d60a4b3ab2037791ee297
Alex de Wekker <alexdewekker6@gmail.com>
2018-12-31 15:18:12 +0100

    doing things

--- projects/speedtest/speedtest.js
+++ projects/speedtest/speedtest.js
@@ -1,6 +1,4 @@
 // ----------------------- TO DO ------------------------
-// ----------------------- TO DO ------------------------
-// ----------------------- TO DO ------------------------
 //
 // DONE == Debug photo styling issue chrome mobile.
 // Add some test info from the db.
@@ -11,54 +9,51 @@
 // plus 60 pixels for the chrome bar.
 // div2 is 60 pixels longer than div1 and div1 is overflow hidden.
 
-
-// -------------------- SET UP STUFF --------------------
-// -------------------- SET UP STUFF --------------------
 // -------------------- SET UP STUFF --------------------
 
 // Required packages
-const express = require("express");
-const favicon = require("serve-favicon");
-const mongodb = require("mongodb");
-const path = require("path");
+const express = require("express")
+const favicon = require("serve-favicon")
+const mongodb = require("mongodb")
+const path = require("path")
 
-// Declare the app
-const app = express();
-const port = 3009;
+// Declare the ap
+const app = express()
+const port = 3009
 
 // Make sure nothing about the server is put in the header
-app.disable("x-powered-by");
+app.disable("x-powered-by")
 
 // Setup mongodb stuff.
-const MongoClient = mongodb.MongoClient;
-const user = process.env.MONGODB_RWU;
-const ww = process.env.MONGODB_RWP;
-const ip = "192.168.1.90";
-const mongoport = 27017;
-const db = "speedtest";
-const url = "mongodb://" + user + ":" + ww + "@" + ip + ":" + port + "/" + db;
+const MongoClient = mongodb.MongoClient
+const user = process.env.MONGODB_RWU
+const ww = process.env.MONGODB_RWP
+const ip = "192.168.1.90"
+const mongoport = 27017
+const db = "speedtest"
+const url = "mongodb://" + user + ":" + ww + "@" + ip + ":" + port + "/" + db
 
 // View engine setup
-app.set("views", path.join(__dirname, "0-views"));
-app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "0-views"))
+app.set("view engine", "pug")
 // and make the source look nice
-app.locals.pretty = true;
+app.locals.pretty = true
 
 // -------------------- MIDDLEWARE ----------------------
 // -------------------- MIDDLEWARE ----------------------
 // -------------------- MIDDLEWARE ----------------------
 
 // Use your public folder
-app.use(express.static(path.join(__dirname, "1-public")));
+app.use(express.static(path.join(__dirname, "1-public")))
 
 // Favicon
 // Uncomment after placing favicon in public
-app.use(favicon(path.join(__dirname, "1-public", "favicon.ico")));
+app.use(favicon(path.join(__dirname, "1-public", "favicon.ico")))
 
 // Setup jQuery and Bootstrap
 // Maybe moves these to public as sublime cannot see node_modules.
-app.use("/jquery", express.static(__dirname + "node_modules/jquery/dist/"));
-app.use("/bootstrap", express.static(__dirname + "node_modules/bootstrap/dist/"));
+app.use("/jquery", express.static(__dirname + "node_modules/jquery/dist/"))
+app.use("/bootstrap", express.static(__dirname + "node_modules/bootstrap/dist/"))
 
 // -------------------- GET PAGES -----------------------
 // -------------------- GET PAGES -----------------------
@@ -67,12 +62,12 @@ app.use("/bootstrap", express.static(__dirname + "node_modules/bootstrap/dist/")
 // A simple getPage function.
 function getPage(url, view, text) {
     app.get(url, (req, res) => {
-        res.render(view, { text : text });
+        res.render(view, { text : text })
     });
 }
 
 // The homepage.
-getPage("/", "index", "Welcome to SpeedTest.");
+getPage("/", "index", "Welcome to SpeedTest.")
 
 // The temporary results page.
 getPage("/results", "results", "Here are some results.")
@@ -82,7 +77,7 @@ getPage("/results", "results", "Here are some results.")
 // -------------------- ERROR HANDLING ------------------
 
 // 404 page
-getPage("*", "404", "Page not found.");
+getPage("*", "404", "Page not found.")
 
 // -------------------- PORT CONFIG ---------------------
 // -------------------- PORT CONFIG ---------------------
@@ -90,7 +85,7 @@ getPage("*", "404", "Page not found.");
 
 // Open the port for the app to work on
 app.listen(port, () => {
-    console.log("      " + db.toUpperCase() + " is now listening on port: " + port);
+    console.log("      " + db.toUpperCase() + " is now listening on port: " + port)
 });
 
 // -------------------- ============ --------------------
