7355b4e99e8965b68b96f997492713cac5f174a0
Alex de Wekker <alexdewekker6@gmail.com>
2019-01-02 23:56:50 +0100

    added dotenv support

--- projects/urlname/3-routes/index.js
+++ projects/urlname/3-routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { Client } = require("pg")
 const router = express.Router()
+require("dotenv").config({ path: "/var/www/html/backup/projects/urlname/1-public" })
 
 // ---------- MIDDLEWARE ----------
 
