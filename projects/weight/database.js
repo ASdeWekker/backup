09ffac3431771a6ef871e52543e0044f9b166a96
Alex de Wekker <alexdewekker6@gmail.com>
2019-05-25 12:30:18 +0200

    added the date and notes part

--- projects/weight/database.js
+++ projects/weight/database.js
@@ -1,14 +1,14 @@
 const { Client } = require("pg")
 require("dotenv").config({ path: "/var/www/html/backup/projects/weight/1-public/.env" })
-const db = "weight"
 const query = `
 create table if not exists weight (
 	id serial primary key not null
 	weight varchar(5) not null
-	dat stuff)
+	date varchar(10) not null
+	notes varchar(255))
 `
 
-const connStr = "postgres://" + process.env.PSQLU + ":" + process.env.PSQLW + "@192.168.1.90:5432/" + db
+const connStr = "postgres://" + process.env.PSQLU + ":" + process.env.PSQLW + "@192.168.1.90:5432/weight"
 const client = new Client({ connectionString: connStr })
 client.connect()
 client.query(query).then(data => {
