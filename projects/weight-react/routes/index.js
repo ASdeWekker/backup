419747ad5c9da49fc86cad66992f0b7f1291022c
Alex de Wekker <alexdewekker6@gmail.com>
2019-10-01 22:56:09 +0200

    added a working GET route, POST will be added tomorrow

--- projects/weight-react/routes/index.js
+++ projects/weight-react/routes/index.js
@@ -1,6 +1,21 @@
+const express = require("express")
+const router = express.Router()
+const { Pool } = require("pg")
 
+const connStr = "postgresql://" + process.env.PSQLU + ":" + process.env.PSQLW + "@192.168.1.90:5432/weight"
+
+const pool = new Pool({ connectionString: connStr })
+pool.connect()
+
+const getWeight = "select * from weight order by id desc"
+const postWeight = "insert into weight (weight_val, date, notes) value ($1, $2, $3)"
 
 // Weight routes
-app.route("/api/weight")
-	.get(weight.getList)
-	.post(weight.postWeight)
+router.route("/weight")
+	.get((req, res) => {
+		pool.query(getWeight)
+			.then(data => res.json(data))
+			.catch(e => console.error(e.stack))
+	})
+
+module.exports = router
--- projects/weight-react/weight.js
+++ projects/weight-react/weight.js
@@ -1,17 +1,24 @@
 const express = require("express")
 
+const indexRouter = require("./routes/index.js")
+
 const app = express()
 
+app.disable("x-powered-by")
 
-app.get("/api/weight", (req, res) => {
-    const weight = [
-        {id: 1, weight: "100", date: "21-09-2019"},
-        {id: 2, weight: "105", date: "11-08-2019"},
-        {id: 3, weight: "110", date: "03-07-2019"}
-    ]
 
-    res.json(weight);
-})
+// app.get("/api/weight", (req, res) => {
+//     const weight = [
+//         {id: 1, weight: "100", date: "21-09-2019"},
+//         {id: 2, weight: "105", date: "11-08-2019"},
+//         {id: 3, weight: "110", date: "03-07-2019"}
+//     ]
+
+//     res.json(weight);
+// })
+
+
+app.use("/api", indexRouter)
 
 
 module.exports = app
