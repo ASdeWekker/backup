af691474ac2299c40c6c46e723be445560e902ba
Alex de Wekker <alexdewekker6@gmail.com>
2019-10-16 21:57:48 +0200

    now able to GET data from the API

--- projects/weight-react/client/src/components/List.js
+++ projects/weight-react/client/src/components/List.js
@@ -8,18 +8,22 @@ class List extends Component {
 		}
 	}
 
-	// componentDidMount() {
-	// 	fetch("/api/weight")
-	// 		.then(res => res.json())
-	// 		.then(weight => this.setState({weight}, () => console.log("Weight fetched...", weight)))
-	// }
+	componentDidMount() {
+		fetch("/api/weight")
+			.then(res => res.json())
+			.then(weight => this.setState({weight}, () => console.log("Weight fetched...", weight)))
+	}
 
 	render() {
 		return (
 			<div>
 				<ul>
 				{this.state.weight.map(weight =>
-					<li key={weight.id}>{weight.weight} {weight.date}</li>
+					<li key={weight.id}>
+						<p>{weight.weight_val}</p>
+						<p>{weight.date}</p>
+						<p>{weight.notes}</p>
+					</li>
 				)}
 				</ul>
 			</div>
