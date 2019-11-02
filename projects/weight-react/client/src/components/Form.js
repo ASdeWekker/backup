c096ecfbdc5a9ef1d8f927b5f0a368116f1bf1e9
Alex de Wekker <alexdewekker6@gmail.com>
2019-11-02 14:10:01 +0100

    expanded the inputs and adding a react form method

--- projects/weight-react/client/src/components/Form.js
+++ projects/weight-react/client/src/components/Form.js
@@ -1,16 +1,61 @@
 import React, { Component } from "react"
 
 class Form extends Component {
+	constructor(props) {
+		super()
+		this.state = {
+			values: {
+				weight_val: "",
+				notes: ""
+			},
+			isSubmitting: false,
+			isError: false
+		}
+	}
+
+	handleInputChange = e => {
+		this.setState({
+			values: { ...this.state.values, [e.target.name]: e.target.value }
+		})
+	}
+
+	submitForm = e => {
+		e.preventDefault()
+		console.log(this.state.values)
+	}
+
 	render() {
 		return (
-			<form className="form" action="/api/weight" method="POST">
+			<form className="form" onSubmit={this.submitForm}>
 				<div className="form--row weight-input">
-					<label className="form--row--label" for="weight_val"></label>
-					<input className="form--row--input" type="number" placeholder="Gewicht..." name="weight_val" id="weight_val" min="0" step="any" required />
+					<label className="form--row--label" htmlFor="weight_val"></label>
+					<input
+						className="form--row--input"
+						value={this.state.values.weight_val}
+						onChange={this.handleInputChange}
+						type="number"
+						placeholder="Gewicht..."
+						name="weight_val"
+						id="weight_val"
+						title="Weight"
+						min="0"
+						step="any"
+						required
+					/>
 				</div>
 				<div className="form--row notes-input">
-					<label className="form--row--label" for="notes"></label>
-					<input className="form--row--input" type="text" placeholder="Notities..." name="notes" id="notes" maxlength="255" />
+					<label className="form--row--label" htmlFor="notes"></label>
+					<input
+						className="form--row--input"
+						value={this.state.values.notes}
+						onChange={this.handleInputChange}
+						type="text"
+						placeholder="Notities..."
+						name="notes"
+						id="notes"
+						title="Notes"
+						maxlength="255"
+					/>
 				</div>
 				<div className="form--row">
 					<button className="form--row--button" type="submit">Toevoegen</button>
