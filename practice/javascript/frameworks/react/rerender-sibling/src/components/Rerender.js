7e18a78d753d626375f64b8c8fbc7bb9f7cf3475
Alex de Wekker <alexdewekker6@gmail.com>
2019-11-20 20:39:50 +0100

    finished the sibling rerender example

--- practice/javascript/frameworks/react/rerender-sibling/src/components/Rerender.js
+++ practice/javascript/frameworks/react/rerender-sibling/src/components/Rerender.js
@@ -4,19 +4,43 @@ const A = ({ users, selectUser }) => {
 	return (
 		<React.Fragment>
 			<h1>I am A.</h1>
-			{users.map(u, i) => {
+			{users.map((u, i) => {
 				return <button onClick={() => selectUser(i)}>{u}</button>
 			})}
 		</React.Fragment>
 	)
 }
 
-// Const B goes here.
+const B = ({ user }) => {
+	return <h1>I am B. Current user: {user}</h1>
+}
+
+const C = ({ user }) => {
+	return <h1>I am C. Current user: {user}</h1>
+}
 
 class Rerender extends Component {
+	state = {
+		users: ["Bob", "Anne", "Mary"],
+		currentUserIndex: 0
+	}
+
+	selectUser = n => {
+		this.setState({
+			currentUserIndex: n
+		})
+	}
+
 	render() {
+		const { users, currentUserIndex } = this.state
+		const currentUser = users[currentUserIndex]
+
 		return (
-			<h1>Ding</h1>
+			<React.Fragment>
+				<A selectUser={this.selectUser} users={users} />
+				<B user={currentUser} />
+				<C user={currentUser} />
+			</React.Fragment>
 		)
 	}
 }
