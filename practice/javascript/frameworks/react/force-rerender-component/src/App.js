af105a2fbab7fecea8a05eee0f1d5493bfc559ca
Alex de Wekker <alexdewekker6@gmail.com>
2019-11-27 21:58:20 +0100

    couldn't get much work done, practice thing not yet working

--- practice/javascript/frameworks/react/force-rerender-component/src/App.js
+++ practice/javascript/frameworks/react/force-rerender-component/src/App.js
@@ -1,12 +1,30 @@
-import React from "react"
+import React, { Component } from "react"
 import "./App.css"
+import SiblingOne from "./components/SiblingOne"
+import SiblingTwo from "./components/SiblingTwo"
 
-function App() {
-	return (
-		<div className="App">
-			<h1>Ding</h1>
-		</div>
-	)
+class App extends Component {
+	constructor(props) {
+		super()
+		this.state = {
+			values: {
+				value: ""
+			}
+		}
+	}
+
+	handleUpdate = () => {
+		this.forceUpdate()
+	}
+
+	render() {
+		return (
+			<div>
+				<SiblingOne />
+				<SiblingTwo />
+			</div>
+		)
+	}
 }
 
 export default App
