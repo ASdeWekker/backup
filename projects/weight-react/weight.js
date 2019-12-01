ad436004ef0efaeebb517d419db425bbaed1cdef
Alex de Wekker <alexdewekker6@gmail.com>
2019-12-01 18:03:14 +0100

    removed the CORS bit as it was not needed after all

--- projects/weight-react/weight.js
+++ projects/weight-react/weight.js
@@ -17,13 +17,6 @@ app.disable("x-powered-by")
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: false }))
 
-// Allow the react front-end via CORS.
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "http://10.8.0.4:4010")
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-	next()
-})
-
 
 // Call the routes.
 app.use("/api", indexRouter)
