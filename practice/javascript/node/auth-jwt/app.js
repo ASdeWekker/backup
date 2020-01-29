30edccc638fbb4796c7c05d51b308c789e8ce5ec
Alex de Wekker <alexdewekker6@gmail.com>
2020-01-29 21:25:16 +0100

    added port variable, reformatted mailOptions, restructured /login route, shortened token life

--- practice/javascript/node/auth-jwt/app.js
+++ practice/javascript/node/auth-jwt/app.js
@@ -5,6 +5,8 @@ const cookieparser = require("cookie-parser")
 const nodemailer = require("nodemailer")
 require("dotenv").config({ path: "/var/www/html/penp/practice/javascript/node/auth-jwt/.env" })
 
+const port = 3012
+
 let transporter = nodemailer.createTransport({
 	host: process.env.MAILHOST,
 	port: process.env.MAILPORT,
@@ -29,7 +31,7 @@ function auth(req, res, next) {
 		let privateKey = process.env.SECRETKEY
 		jwt.verify(token, privateKey, (err, user) => {
 			if (err) {
-				res.status(403).json({ "message": "Not authorized" })
+				res.status(403).json({ "message": "You're not authorized" })
 				console.log(err)
 			}
 			console.log(jwt.decode(token))
@@ -45,38 +47,35 @@ app.get("/", auth, (req, res) => {
 	res.json({ "message":  "hello world, you need a token to get in here." })
 })
 
-app.route("/login").get((req, res) => {
-	res.json({ "message": "Enter your e-mail adres and we will send you a token" })
-}).post((req, res) => {
-	let email = req.body.email
-	let privateKey = process.env.SECRETKEY
-	let token = jwt.sign({ expiresIn: "1h", email: email }, privateKey, { algorithm: "HS512" })
-	let mailOptions = {
-		from: "test@dewekker.dev",
-		to: email,
-		subject: "Hier is je token.",
-		html: "Hier is de link!Log maar lekker in bij localhost: http://localhost:3000/verify/" + token +
-		"Bij pcaats: http://10.8.0.4:3000/verify/" + token +
-		"Of bij servertjih: http://10.8.0.5:3000/verify/" + token,
-		html: "<p>Hier is de link!<br />Log maar lekker in bij localhost: <a href='http://localhost:3000/verify/" + token + "'>http://localhost:3000/verify/" + token + "</a><br /> \
-		Bij pcaats: <a href='http://10.8.0.4:3000/verify/" + token + "'>http://10.8.0.4:3000/verify/" + token + "</a><br /> \
-		Of bij servertjih: <a href='http://10.8.0.5:3000/verify/" + token + "'>http://10.8.0.5:3000/verify/" + token + "</a></p>"
-	}
-	transporter.sendMail(mailOptions, (err, info) => {
-		if (err) {
-			console.log(err)
-			res.json({ "message": "Something went wrong..." })
-		} else {
-			console.log(info)
-			res.json({ "message": "Token has been sent via mail" })
+app.route("/login")
+	.get((req, res) => {
+		res.json({ "message": "Enter your e-mail adres and we will send you a token" })})
+	.post((req, res) => {
+		let email = req.body.email
+		let privateKey = process.env.SECRETKEY
+		let token = jwt.sign({ expiresIn: "10m", email: email }, privateKey, { algorithm: "HS512" })
+		let mailOptions = {
+			from: "info@dewekker.dev",
+			to: email,
+			subject: "Hier is je token.",
+			text: `Hier is de link! Log maar lekker in bij localhost: http://localhost:${port}/verify/${token} Bij aad: http://10.8.0.4:${port}/verify/${token} Of bij serge: http://10.8.0.5:${port}/verify/${token}`,
+			html: `<p>Hier is de link!<br />Log maar lekker in bij <a href='http://localhost:${port}/verify/${token}'>localhost</a></p><p>Bij: <a href='http://10.8.0.4:${port}/verify/${token}'>aad</a></p><p>Of bij: <a href='http://10.8.0.5:${port}/verify/${token}'>serge</a></p><p>De link is als volgt: <pre>/verify/${token}</pre></p>`
+
 		}
-	})
-})
+		transporter.sendMail(mailOptions, (err, info) => {
+			if (err) {
+				console.log(err)
+				res.json({ "message": "Something went wrong..." })
+			} else {
+				console.log(info)
+				res.json({ "message": "Token has been sent via mail" })
+			}
+		})})
 
 app.get("/verify/:token", (req, res) => {
-	res.cookie("token", req.params.token, { maxAge: 1000 * 60 * 60, secure: true, httpOnly: true }).redirect("/")
+	res.cookie("token", req.params.token, { maxAge: 1000 * 60 * 10, secure: true, httpOnly: true }).redirect("/")
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log("App is running")
 })
